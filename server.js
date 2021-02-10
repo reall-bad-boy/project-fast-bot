92099f22ef24ca51b773d04412f378767d59a787
Glitch (pickled-enchanting-ship) <none>
2021-02-10 19:01:34 +0000

    🐂😋 Checkpoint ./server.js:23209798/699

--- server.js
+++ server.js
@@ -1660,20 +1660,3 @@ client.on('message',async message => {
 
 
 
-client.on("guildMemberAdd", member => {
-  let m = member.user;
-  var embed = new Discord.MessageEmbed()
-  .setTitle("بەخێربێیت بۆ سێرڤەرەکەمان دڵم تکایە ڕێزی خۆت بگرە")
- .addField("ناو:", member.user)
-  .addField("ئایدی ئەندام:", member.id) 
-  .addField("تۆ کەسی ژمارە:", member.guild.memberCount)
-  .setThumbnail(m.avatarURL())
-  .setImage("https://cdn.discordapp.com/attachments/712441079055777895/712441107660800020/image0.jpg")
-   .setColor("RANDOM")
-    .setFooter(`${member.user.username}`, member.user.avatarURL()) 
-    .setTimestamp();
-  var channel = member.guild.channels.cache.get("809028885923627049");
-  if (!channel) return;
-  channel.send({ embed: embed });
-});
- 
